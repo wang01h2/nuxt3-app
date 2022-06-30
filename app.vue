61618c7dd94b61bffa2da7c15e98c847421a83ce
wang01h2 <1299839398@qq.com>
2022-06-30 16:15:37 +0800

    fix(app.vue): 搭建模板
    搭建模板

--- app.vue
+++ app.vue
@@ -13,3 +13,26 @@ useHead({
 <template>
   <NuxtPage />
 </template>
+
+<style>
+@media (prefers-color-scheme: dark) {
+  html {
+    background-color: #101827;
+
+  }
+
+  .todo-view {
+    background-color: #101827;
+  }
+}
+
+#__nuxt {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+body {
+  background-color: #F9FAFB;
+}
+</style>
